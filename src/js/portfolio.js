b0bc9b47e100c0306112afacfb9e70ce94a567cd
Evgeny-Tokarev <ghenya.t@gmail.com>
2020-07-26 23:47:06 +0300

    multiswiper init

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -4,3 +4,4 @@
       import { menuOperating } from "./menuOperating.js";
 
       menuOperating(prototipMenuItems, prototipItems, "white-bg", "black-bg", "swiper-shown");
+      export  {prototipItems};
